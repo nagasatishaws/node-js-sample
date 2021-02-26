acd7eb50e50613a0c6c35c9acd51b404a056cddd
nagasatishaws <64299813+nagasatishaws@users.noreply.github.com>
2021-02-26 22:41:12 +0530

    aws services

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('HI THIS IS NAGASATISH,AWS SOLUTION ARCHITECT ASSOSIATE')
+  response.send('HI THIS IS NAGASATISH,AWS SOLUTION ARCHITECT ASSOSIATE...COURSE CONTENT ....ec2,vpc,rds,ses,sns,iam,cft,elb,ebs,firewall,waf,codebuild,codepipeline,codecommit,ecs,ecr,fargate,awsbackup,route53,clouwatch,cloudtrail,s3...')
 }) 
 
 app.listen(app.get('port'), function() {
